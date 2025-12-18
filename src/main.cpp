3da4329fcaa25fd99f111ba879f6440da09394eb
nicolae.tabirca <nicolae.tabirca@stud.acs.upb.ro>
2025-12-18 23:51:03 +0200

    taskfive

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
+#include <ranges>
 #include <string>
+#include <sstream>
 
 int main()
 {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string line;
   std::string command;
 
   while (1) {
     std::cout << "$ ";
-    std::cin >> command;
+    std::getline(std::cin, line);
+    std::stringstream ss(line);
+    ss >> command;
     if (command == "echo")
     {
-      std::string echo;
-      std::cin >> echo;
-      std::cout << echo << std::endl;
+      std::string word;
+      while (ss >> word)
+        std::cout << word << " ";
+      std::cout << std::endl;
     }
     else if (command == "exit")
       break;
