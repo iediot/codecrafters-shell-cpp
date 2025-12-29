5cbcfe3dfbb2315d4640166b80c2d7a1b84ea24c
nicolae.tabirca <nicolae.tabirca@stud.acs.upb.ro>
2025-12-29 17:24:38 +0200

    added history

--- src/main.cpp
+++ src/main.cpp
@@ -471,8 +471,8 @@ int main() {
             }
 
             else if (cmd == "history") {
-                for (int i = 1; i <= history_index; i++) {
-                    std::cout << i << "  " << history[i] << "\n";
+                for (int i = 0; i < history_index; i++) {
+                    std::cout << i + 1 << "  " << history[i] << "\n";
                 }
             } else {
                 std::vector<char*> c_args;
